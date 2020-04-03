f7934cfe87be3162b1181d1e9bde2f39d8503128
kur <rhdnfka94@gmail.com>
2020-04-03 13:12:29 +0900

    Add item seq and rand seq

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 // map <F4> :wa<CR>:!g++ main.cpp cbuf.c -o main;./main<CR>
 
 #include <iostream>
-#include "cbuf.h"
 #include <vector>
+#include <numeric>   // iota
+#include <algorithm> // shuffle
+#include <random>    // mt, rand_dev
+#include "cbuf.h"
+
+std::vector<Item> item_seq(Item beg, Item end){
+    std::vector<Item> vec(end);
+    std::iota(vec.begin(), vec.end(), beg);
+    return vec;
+}
+
+std::vector<Item> item_rand_seq(Item beg, Item end){
+    auto vec = item_seq(beg, end);
+    std::shuffle(
+        vec.begin(), vec.end(), 
+        std::mt19937{std::random_device{}()});
+    return vec;
+}
 
 int main(int argc, char* argv[]){
-    std::cout << "ppap" << std::endl;
+    //auto vec = item_seq(0, 30);
+    auto vec = item_rand_seq(0, 30);
+    for(int i = 0; i < 30; i++){
+        std::cout << vec[i] << ' ';
+    }
     return 0;
 }
